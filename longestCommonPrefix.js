ca35f568f1939f28d9c808b237df0cb1cd3a1e30
Tofik Hasanov <annexcies@gmail.com>
2024-01-17 19:39:53 +0400

    longestCommonPrefix

---
+++ longestCommonPrefix.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  const MAX_LENGTH = strs.length,
+    first_el = strs[0];
+
+  for (let i = 0; i < first_el.length; i++) {
+    const char = first_el.charAt(i);
+
+    let idx = 1;
+
+    while (idx < MAX_LENGTH) {
+      const el = strs[idx];
+      if (i === el.length || el.charAt(i) != char) {
+        return first_el.substring(0, i);
+      }
+      idx++;
+    }
+  }
+  return first_el;
+};
