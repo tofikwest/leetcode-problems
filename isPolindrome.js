ffb94cc4338119bca1552eb621847ee21a6146e9
Tofik Hasanov <annexcies@gmail.com>
2024-01-17 12:35:51 +0400

    isPolindrome solution

---
+++ isPolindrome.js
@@ -0,0 +1,16 @@
+var isPalindrome = function (x) {
+  x = x.toString();
+  let left = 0,
+    right = x.length - 1;
+
+  while (left < right) {
+    if (x[left] !== x[right]) return false;
+
+    left++;
+    right--;
+  }
+
+  return true;
+};
+
+isPalindrome(121);
