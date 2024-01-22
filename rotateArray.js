4f982dd9d7709504b6703a24e2001855eada828a
Tofik Hasanov <annexcies@gmail.com>
2024-01-22 12:19:04 +0400

    write my step names

--- rotateArray.js
+++ rotateArray.js
@@ -6,17 +6,17 @@
 
 function reverse(nums, start, end) {
   while (start < end) {
-      let temp = nums[start]
-      nums[start] = nums[end]
-      nums[end] = temp
-      start++, end--;
+    let temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++, end--;
   }
 }
 
 var rotate = function (nums, k) {
   k %= nums.length;
 
-  reverse(nums, 0, nums.length - 1);
-  reverse(nums, 0, k - 1);
-  reverse(nums, k, nums.length - 1);
+  reverse(nums, 0, nums.length - 1); // полностью перевернули массив
+  reverse(nums, 0, k - 1); // сделали поворот на k
+  reverse(nums, k, nums.length - 1); // дописали остальное
 };
